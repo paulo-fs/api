b9923b7d53e94e2befd8374eacff1062ac731aa9
paulo-fs <jr.paulo87@gmail.com>
2024-02-18 16:05:32 -0300

    feat: make users routes public

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,8 +1,10 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { IsPublic } from 'src/shared/decorators/IsPublic';
 import { AuthService } from './auth.service';
 import { SigninDto } from './dto/signin.dto';
 import { SignUpDto } from './dto/signup.dto';
 
+@IsPublic()
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
