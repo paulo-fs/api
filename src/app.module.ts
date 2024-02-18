d0fb60c895dad679d17892a7735acf300ab0bc6e
paulo-fs <jr.paulo87@gmail.com>
2024-02-18 10:29:59 -0300

    fix: add jwt service in providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,16 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
+import { AuthGuard } from './modules/auth/auth.guard';
+import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 import { DatabaseModule } from './shared/database/database.module';
-import { AuthModule } from './modules/auth/auth.module';
-import { APP_GUARD } from '@nestjs/core';
-import { AuthGuard } from './modules/auth/auth.guard';
-import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, AuthModule, JwtModule],
+  imports: [UsersModule, DatabaseModule, AuthModule],
   controllers: [],
   providers: [
+    JwtService,
     {
       provide: APP_GUARD,
       useClass: AuthGuard,
